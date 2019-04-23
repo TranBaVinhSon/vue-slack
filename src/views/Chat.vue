84a5cf8c0527cd9867661193919f405b488e1ff9
TranBaVinhSon <tran.ba.vinh.son@mf-financial.jp>
2019-04-23 20:51:19 +0900

    logout

--- src/views/Chat.vue
+++ src/views/Chat.vue
@@ -1,13 +1,34 @@
 <template lang="html">
   <div>
     <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-4">
+          <button @click="logout" class="btn btn-outline-primary">Logout</button>
+        </div>
 
+        <div class="col-md-8">
+          content
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import auth from 'firebase/auth';
+export default {
+  name: 'chat',
+  methods: {
+    logout() {
+      console.log("logout")
+      firebase.auth().signOut()
+      this.$store.dispatch('setUser', null)
+      this.$router.push({
+        path: '/login'
+      })
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
